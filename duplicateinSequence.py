fef214e8e5ac22ed7a60ff3b98747057780dafd7
Sai Manikanta <infomanikanta@gmail.com>
2016-09-28 23:44:48 -0500

    Update and rename duplicate2.py to duplicateinSequence.py
    Comments, File name Changes

--- duplicateinSequence.py
+++ duplicateinSequence.py
@@ -5,7 +5,8 @@ def list_duplicates(seq):
   seen_twice = set( x for x in seq if x in seen or seen_add(x) )
   # turn the set into a list (as requested)
   return list( seen_twice )
-
+#Input Sequence
 seq = [1,1,2,2,3,4,6,7]
+#Calling the function to list all duplicates
 list_duplicates(seq)
 print (list_duplicates(seq))
