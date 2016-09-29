8990049cd995ac7a0b7f3548b8d8bb1f371afb24
Sai Manikanta <infomanikanta@gmail.com>
2016-09-29 17:51:47 -0500

    Update inverseValuesofListandSum.py
    Comments Updated

--- inverseValuesofListandSum.py
+++ inverseValuesofListandSum.py
@@ -1,10 +1,14 @@
+# Take raw input values from user in the format as 1 2 3 4 etc
 string_input = raw_input()
 input_list = string_input.split() #splits the input string on spaces
 # process string elements in the list and make them integers
 myList = [int(a) for a in input_list]
+# Printing the list just to check whether they are converted into list
 print (myList)
-myInt = 10.0
+myInt = 1.0
+#Inverse the list, Remember we sre taking myInt values as 1, which is 1/ each value
 newList = [myInt / x for x in myList]
 myList[:] = [myInt / x  for x in myList]
+#Sum all the inverse values in the list as follows
 sumx=sum(newList)
 print (sumx)
